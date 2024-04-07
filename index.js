bbd89c600ff3e493ae7cbf3891e1296f627aca8e
David Ward <professorx00@gmail.com>
2024-04-07 09:36:03 -0400

    Initalized Node Script and Added basic thoughts on steps need to complete.

---
+++ index.js
@@ -0,0 +1,13 @@
+//Steps that need to happen:
+
+//Request Folder directory of the unzipped export files
+
+//Search files for JSON files
+
+//Once Found Convert JSON to Scabard Format
+
+//Request API Key and User Name
+
+//Request Campaign Id for importing
+
+// Once all are converted Process the Imports
